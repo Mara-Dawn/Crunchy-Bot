c46026d61f696c1d120e1e249ebfde7d1443cecd
Mara Dawn <MaraDawn69@Gmail.com>
2024-01-27 15:36:47 +0100

    Quote improvements

--- cogs/Quotes.py
+++ cogs/Quotes.py
@@ -46,7 +46,7 @@ class Quotes(commands.Cog):
         quote_id = self.database.log_quote(quote)
         
         self.event_manager.dispatch_quote_event(message.created_at, guild_id, quote_id)
-        response = 'Quote was sucessfully saved.'
+        response = f'<@{message.author.id}> Your message {message.jump_url} was deemed quote worthy by <@{interaction.user.id}> and will be saved for the future.'
         await self.bot.response(self.__cog_name__, interaction, response, 'quote', interaction.user.display_name, message.author.display_name)
     
     @commands.Cog.listener()
