60ac2f4228a267fec5643efa8ca8a04ca2459cf0
Mara Dawn <MaraDawn69@Gmail.com>
2024-04-14 19:56:11 +0200

    fixed dumb bug

--- view/prediction_view.py
+++ view/prediction_view.py
@@ -97,7 +97,7 @@ class PredictionView(ViewMenu):
             self.predictions,
             key=lambda x: (
                 PredictionStateSort.get_prio(x.prediction.state),
-                x.prediction.get_timestamp(),
+                x.prediction.get_timestamp_sort(),
                 -x.prediction.id,
             ),
         )
